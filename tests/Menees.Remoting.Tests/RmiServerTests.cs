121330aa8067cb412a6b4fc16a0eeaf9b42880cf
Bill Menees <Bill@Menees.com>
2022-03-02 20:24:09 -0600

    Added RMI security unit test

--- tests/Menees.Remoting.Tests/RmiServerTests.cs
+++ tests/Menees.Remoting.Tests/RmiServerTests.cs
@@ -3,7 +3,11 @@
 #region Using Directives
 
 using System.Diagnostics;
+using System.IO.Pipes;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Menees.Remoting.Pipes;
+using Menees.Remoting.Security;
 
 #endregion
 
@@ -100,7 +104,31 @@ public class RmiServerTests : BaseTests
 			4).ConfigureAwait(false);
 	}
 
-	// TODO: Add RMI test with client and server security. [Bill, 2/20/2022]
+	[TestMethod]
+	public void SecurityVariations()
+	{
+		const int ClientCount = 2;
+		this.TestClient(1, 1, ClientCount, PipeClientSecurity.CurrentUserOnly);
+		this.TestClient(1, 1, ClientCount, serverSecurity: PipeServerSecurity.CurrentUserOnly);
+		this.TestClient(1, 1, ClientCount, PipeClientSecurity.CurrentUserOnly, PipeServerSecurity.CurrentUserOnly);
+
+		if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+		{
+			try
+			{
+				// This empty PipeSecurity instance doesn't grant any user access to the pipe (even the current user).
+				PipeSecurity pipeSecurity = new();
+				this.TestClient(1, 1, ClientCount, PipeClientSecurity.CurrentUserOnly, new PipeServerSecurity(pipeSecurity));
+				Assert.Fail("Client should not have access to connect to server.");
+			}
+			catch (AggregateException ex)
+			{
+				// Depending on how TestClient's Parallel.ForEach fails, we may get 1 or 2 UnauthorizedAccessExceptions.
+				ex.InnerExceptions.Any(e => e is UnauthorizedAccessException).ShouldBeTrue();
+			}
+		}
+	}
+
 	#endregion
 
 	#region Private Methods
@@ -109,19 +137,36 @@ public class RmiServerTests : BaseTests
 		int minServerListeners,
 		int maxServerListeners,
 		int clientCount,
+		ClientSecurity? clientSecurity = null,
+		ServerSecurity? serverSecurity = null,
 		[CallerMemberName] string? callerMemberName = null)
 	{
 		string serverPath = this.GenerateServerPath(callerMemberName);
 
-		Tester tester = new();
-		using RmiServer<ITester> server = new(tester, serverPath, maxServerListeners, minServerListeners, loggerFactory: this.Loggers);
-		server.ReportUnhandledException = WriteUnhandledServerException;
-		server.Start();
+		ServerSettings serverSettings = new(serverPath)
+		{
+			MaxListeners = maxServerListeners,
+			MinListeners = minServerListeners,
+			LoggerFactory = this.Loggers,
+			Security = serverSecurity,
+		};
 
-		this.TestClient(clientCount, serverPath);
+		Tester tester = new();
+		using ServerHost host = new();
+		using RmiServer<ITester> server = new(tester, serverSettings);
+		server.ReportUnhandledException = WriteUnhandledServerException;
+		host.Add(server);
+
+		this.TestClient(clientCount, serverPath, clientSecurity);
+
+		// Make sure all the servers have completely exited in case another TestClient
+		// starts up immediately using the same serverPath. We don't want a new client
+		// to race in an grab an old server listener just as its shutting down.
+		((IServerHost)host).Exit();
+		host.WaitForExit();
 	}
 
-	private void TestClient(int clientCount, string serverPath)
+	private void TestClient(int clientCount, string serverPath, ClientSecurity? clientSecurity = null)
 	{
 		TimeSpan timeout = Debugger.IsAttached ? Timeout.InfiniteTimeSpan : ClientSettings.DefaultConnectTimeout;
 		Parallel.ForEach(
@@ -129,9 +174,16 @@ public class RmiServerTests : BaseTests
 			new ParallelOptions { MaxDegreeOfParallelism = Math.Min(clientCount, 8 * Environment.ProcessorCount) },
 			item =>
 			{
-				using RmiClient<ITester> client = new(serverPath, connectTimeout: timeout, loggerFactory: this.Loggers);
+				ClientSettings clientSettings = new(serverPath)
+				{
+					ConnectTimeout = timeout,
+					LoggerFactory = this.Loggers,
+					Security = clientSecurity,
+				};
+
+				using RmiClient<ITester> client = new(clientSettings);
 				ITester proxy = client.CreateProxy();
-				RmiClientTests.TestProxy(proxy, item, isSingleClient: clientCount == 1);
+				TestProxy(proxy, item, isSingleClient: clientCount == 1);
 
 				const string Prefix = "Item";
 				string actual = proxy.Combine(Prefix, item.ToString());
